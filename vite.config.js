0cda15d290740e6370f0472990511fe7f6dd0c47
edjvc <181752503+edjvc@users.noreply.github.com>
2025-08-19 17:46:36 +0800

    Add vitest

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,11 @@
+/// <reference types="vitest/config" />
 import { defineConfig } from 'vite';
 
 export default defineConfig({
   // base: '/vanilla-vite-template/',
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.js',
+  },
 });
